ea8677c9ee139bf7f00a731fb5d1428d22f8d0c2
caiquesandrade <caiquesandrade@live.com>
2020-05-04 00:58:36 -0300

    Databases

---
+++ movies-api-BD.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+-- Servidor:                     192.168.10.10
+-- Versão do servidor:           5.7.29-0ubuntu0.18.04.1 - (Ubuntu)
+-- OS do Servidor:               Linux
+-- HeidiSQL Versão:              11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
